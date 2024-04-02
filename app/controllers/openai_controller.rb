652ac9ebd698d194738fc4cbe7f5be43576b7d1a
Daniel Burgess <dan-burgess@hotmail.com>
2024-04-03 01:03:12 +1030

    update input to a dropdown to select genre for script

--- app/controllers/openai_controller.rb
+++ app/controllers/openai_controller.rb
@@ -1,5 +1,23 @@
 class OpenaiController < ApplicationController
   def index
+    @prompts = [
+      {
+        title: "Comedy",
+        text: "write a script for a movie scene that goes for no more than 1000 words. the movie is an action comedy involving a good guy named BRETT, and sidekick named TERRY and a bad guy name WOBOBO. make it funny. do not include any narration, only briefly describe the scene. start each characters line with their name in capital letters, then '::' after. start and finish each narration or description with '*'"
+      },
+      {
+        title: "Drama",
+        text: "write a script for a movie scene that goes for no more than 1000 words. the movie is a drama involving a man named JAMES, a friend named LISA and a woman name ALICE. make it emotional. do not include any narration, only briefly describe the scene. start each characters line with their name in capital letters, then '::' after. start and finish each narration or description with '*'"
+      },
+      {
+        title: "Horror",
+        text: "write a script for a movie scene that goes for no more than 1000 words. the movie is a horror involving a Woman name JESS, a man named BOB, and another woman named LUCY. make it scary. do not include any narration, only briefly describe the scene. start each characters line with their name in capital letters, then '::' after. start and finish each narration or description with '*'"
+      },
+      {
+        title: "Romance",
+        text: "write a script for a movie scene that goes for no more than 1000 words. the movie is a romance involving a Woman name BELLA and a woman named TAYLA. make it romantic. do not include any narration, only briefly describe the scene. start each characters line with their name in capital letters, then '::' after. start and finish each narration or description with '*'"
+      }
+    ]
     if params[:query]
       service = OpenaiService.new
 
