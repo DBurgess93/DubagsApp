c4e6d3a1a3e26d3f02b56f22dcd2950e3b024d9a
Daniel Burgess <dan-burgess@hotmail.com>
2024-04-11 22:25:55 +0930

    add to gemfile.lock - platform x86_64-linux

--- app/controllers/openai_controller.rb
+++ app/controllers/openai_controller.rb
@@ -15,10 +15,22 @@ class OpenaiController < ApplicationController
     @genre = params[:genre]
     @location = params[:location]
     @characters = params[:characters].split(' ').map(&:strip).map { |name| name.titleize }
+    @villain = params[:villain].split(' ').map(&:strip).map { |name| name.titleize }
+    query_template = "write a script for a movie scene that goes for no more than 1000 words. The movie is a %{genre}, set in %{location}, involving the characters %{characters}, and a villain named %{villain}. do not include any narration, only briefly describe the scene (no more then 10 words per line). start each characters line with their name in capital letters, then ':' after. Start and finish each narration or description with '*'"
 
-    query_template = "write a script for a movie scene that goes for no more than 1000 words. The movie is a %{genre}, set in %{location}, involving the characters %{characters}. do not include any narration, only briefly describe the scene. start each characters line with their name in capital letters, then ':' after. Start and finish each narration or description with '*'"
+      # Example query:
+      # Write a script for a movie scene that goes for no more than 300 words. Briefly describe the scene at the very start but do not include any narration throughout the script, the main body of the script should only include the characters lines. The scene should follow these guidelines:
+      #   Genre: Action/Comedy
+      #   Antagonist: Adam
+      #   Support Role: Dillon
+      #   Villain: Dan
+      #   Location: Space
+      #   Timeline: Cave man era
 
-    query = query_template % {genre: @genre, location: @location, characters: @characters.join(", ")}
+      #   Do not include things like "Dan and Dillon Laugh", and do not include any actions or behaviours of the characters, dialogue only. Make the script really funny but avoid only using jokes that mock the villain.
+
+
+    query = query_template % {genre: @genre, location: @location, characters: @characters.join(", "), villain: @villain.join(", ")}
     @response = OpenaiService.new.call_text(query)
 
     render :index
@@ -32,11 +44,12 @@ class OpenaiController < ApplicationController
       "Action",
       "Sci-Fi",
       "Fantasy",
-      "Soap Opera"
+      "Soap Opera",
     ]
 
     @locations = [
       "Australia",
+      "Beach",
       "United Kingdom",
       "Europe",
       "Sahara Desert",
